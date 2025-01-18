1c2da078f8d23067e482f2a9795956c483dd001f
Juan Lima <juangustavo@gmail.com>
2025-01-18 19:36:21 -0300

    feat: promotions component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
+import Promotions from "./components/Promotions/Promotions";
 
 function App() {
 
   return (
     <>
       <Header />
+      <Promotions />
     </>
   )
 }
---
+++ src/components/Promotions/Promotions.tsx
@@ -0,0 +1,18 @@
+
+import './promotions.sass'
+
+const Promotions = () => {
+  return (
+    <section id="promotions-container">
+        <div className="shadow">
+            <h1>Venha conhecer nossas promoções</h1>
+            <p className="text-product"><span>50% Off</span> nos produtos</p>
+            <div className='btn-product'>
+                Ver produto
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Promotions
